6075f7cb2d9ccd4ad5faec3bf4d69389706c7ed5
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-10-13 16:44:04 -0600

    Wrote conditional rendering for singleMovie

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,19 @@ import { useState } from 'react';
 import "./App.css"
 import movieData from '../../MockData';
 import Movies from '../Movies/Movies'
-import Card from '../Card/Card';
+
 
 
 function App() {
   const mockData = movieData.movies
   const [movies, setMovies] = useState(mockData);
+  const [singleMovie, setSingleMovie] =useState([]);
+
   return (
     <div className="App">
       <h1>Rancid Tomatillos!</h1> 
-      <Movies movies={movies} />
-      <Card card={Card} />
+      {/* <Movies movies={movies} /> */}
+      {!singleMovie.length ?  <Movies movies={movies} /> : null}
     </div>
   )
 }
