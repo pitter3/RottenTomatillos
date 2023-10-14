b6a7adbdbc64a09bc849539448e61a5cafc492f2
Patrick Eitel <patrickeitel12@gmail.com>
2023-10-14 11:27:47 -0700

    Add an unstyled single movie page, this is a WIP

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,10 +1,25 @@
 //This is a card test
 
 function singleMovie({ singleMovie }) {
-  // console.log(singleMovie)
+  console.log(singleMovie)
   return (
     <div>
-      
+      <h2>{singleMovie[0].title}</h2>
+      <img src={singleMovie[0].backdrop_path} alt="movie picture" style={{ height: '300px', width: '500px' }}/>
+      <p>{singleMovie[0].tagline}</p>
+      <aside>{singleMovie[0].overview}</aside>
+      <aside>
+        <div>
+          <p>{singleMovie[0].average_rating}</p>
+          <br></br>
+          <p>{singleMovie[0].release_date}</p>
+          <br></br>
+          <p>{singleMovie[0].runtime}</p>
+          <div>
+            <p>{singleMovie[0].genres}</p>
+          </div>
+          </div>
+      </aside>
     </div>
   )
   
