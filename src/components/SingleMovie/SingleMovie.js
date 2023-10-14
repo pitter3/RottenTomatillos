c663613c74227a484ad5ca55dc45d285504574d8
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-10-14 14:41:54 -0600

    Add CSS styling and layout to invidivual movie page

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,31 +1,31 @@
-import './'
+import "./SingleMovie.css";
 
 function singleMovie({ singleMovie }) {
   console.log(singleMovie);
   return (
     <div className='single-movie-container'>
-      <h2 className='movie-title'>{singleMovie[0].title}</h2>
-      <img
-        src={singleMovie[0].backdrop_path}
-        alt='movie picture'
-        style={{ height: "300px", width: "500px" }}
-      />
-      <p className='movie-tagline'>{singleMovie[0].tagline}</p>
-      <aside className='movie-overview'>{singleMovie[0].overview}</aside>
-      <aside>
-        <div className='movie-info-container'>
-          <p className='movie-average-rating'>
-            {singleMovie[0].average_rating}
-          </p>
-          <br></br>
-          <p className='movie-release-date'>{singleMovie[0].release_date}</p>
-          <br></br>
-          <p className='movie-runtime'>{singleMovie[0].runtime}</p>
-          <div className='movie-tagline'>
-            <p className='movie-genres-container'>{singleMovie[0].genres}</p>
-          </div>
+      <div className='left-container'>
+        <aside className='movie-overview'>{singleMovie[0].overview}</aside>
+      </div>
+      <div className='middle-container'>
+        <h2 className='movie-title'>{singleMovie[0].title}</h2>
+        <img
+          src={singleMovie[0].backdrop_path}
+          alt='movie picture'
+          style={{ height: "200px", width: "400px" }}
+        />
+        <p className='movie-tagline'>{singleMovie[0].tagline}</p>
+      </div>
+
+      <div className='right-container'>
+        <p className='movie-average-rating'>{singleMovie[0].average_rating}</p>
+        <p className='movie-release-date'>{singleMovie[0].release_date}</p>
+        <p className='movie-runtime'>{singleMovie[0].runtime}</p>
+        <br></br>
+        <div className='movie-genres-container'>
+          <p className='movie-genres'>{singleMovie[0].genres}</p>
         </div>
-      </aside>
+      </div>
     </div>
   );
 }
