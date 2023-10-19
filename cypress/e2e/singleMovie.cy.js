baace40644b9031bb579176b589ee20fc37d4bdb
Patrick Eitel <patrickeitel12@gmail.com>
2023-10-19 15:39:28 -0700

    Finalize testing logic for single movie view. Please read PR note

--- cypress/e2e/singleMovie.cy.js
+++ cypress/e2e/singleMovie.cy.js
@@ -21,6 +21,14 @@ describe("Single Movie user flow", () => {
           fixture: 'moneyplane'
         }
       ).as('MoneyPlane')
+      cy.intercept(
+        "GET",
+        "https://rancid-tomatillos.herokuapp.com/api/v2/movies/585244",
+        {
+          statusCode: 200,
+          fixture: 'istillbelieve'
+        }
+      ).as('IStillBelieve')
       cy.wait('@HomePage').then((interception) => {
         cy.get(".movie-container").children().eq(0).click()
         cy.wait('@MoneyPlane').then((interception) => {
@@ -32,7 +40,17 @@ describe("Single Movie user flow", () => {
           cy.get('.movie-genres').contains("Genres: Action, Fantasy, Science Fiction")
           cy.get('img').should("have.attr", "src").should("eq", "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg");
         })
-        // next movie here in a cy.wait
+        cy.get('button').click()
+        cy.get(".movie-container").children().eq(39).click();
+        cy.wait('@IStillBelieve').then((interception) => {
+          cy.get(".movie-title").contains("I Still Believe");
+          cy.get('.movie-overview').contains("A compelling story of love and hope");
+          cy.get('.movie-tagline').contains("Believe in the power of love.");
+          cy.get('.movie-average-rating').contains("Average Rating: 3.8333333333333335 / 10");
+          cy.get('.movie-release-date').contains("Release Date: 2020-03-12");
+          cy.get('.movie-genres').contains("Genres: Drama, Romance");
+          cy.get('img').should("have.attr", "src").should("eq", "https://image.tmdb.org/t/p/original//21Q8bzu10YF9i4O5amBkJBombYo.jpg");
+        });
       })
     })
 })
