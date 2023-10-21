104d772396a4f2a24890dc90c348d5f605971a93
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-10-21 14:07:42 -0600

    Completed error handling for cypress testing, all passing

--- cypress/e2e/error-handling.cy.js
+++ cypress/e2e/error-handling.cy.js
@@ -1,32 +1,29 @@
-it('should display a helpful message to the user when an error occurs', () => {
-  cy.intercept(
-    "GET",
-    "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
-    {
-      statusCode: 500
-    }
-  ).visit("http://localhost:3000");
-  cy.get(".error-message").should('exist');
-})
+it("should display a helpful message to the user when an error occurs", () => {
+  cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+    statusCode: 500,
+  }).visit("http://localhost:3000");
+  cy.get(".error-message").should(
+    "contain",
+    "This is a bad request,  please try again later."
+  );
+});
 
-it('should display another helpful message to the user when an error occurs', () => {
-  cy.intercept(
-    "GET",
-    "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
-    {
-      statusCode: 400
-    }
-  ).visit("http://localhost:3000");
-  cy.get(".error-message").should('exist');
-})
+it("should display another helpful message to the user when an error occurs", () => {
+  cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+    statusCode: 400,
+  }).visit("http://localhost:3000");
+  cy.get(".error-message").should(
+    "contain",
+    "Sorry, the server is down, please try again later."
+  );
+});
 
-it('should display yet another helpful message to the user when an error occurs', () => {
-  cy.intercept(
-    "GET",
-    "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
-    {
-      statusCode: 300
-    }
-  ).visit("http://localhost:3000");
-  cy.get(".error-message").should('exist');
-})
+it("should display yet another helpful message to the user when an error occurs", () => {
+  cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+    statusCode: 300,
+  }).visit("http://localhost:3000");
+  cy.get(".error-message").should(
+    "contain",
+    "Sorry, an error occured, please refresh page or try again later."
+  );
+});
